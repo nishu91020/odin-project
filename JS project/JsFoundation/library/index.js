3de58956cda77cdab1332d5aebe3fbbf6e11e54a
Nishu Rai <54761473+nishu91020@users.noreply.github.com>
2022-01-19 18:53:04 +0530

    delete functionality implemented in library

--- JS project/JsFoundation/library/index.js
+++ JS project/JsFoundation/library/index.js
@@ -1,21 +1,8 @@
-var library = [
-    {
-        name: 'Power of subconsious mind',
-        author: 'J K Rowling',
-        page: 200,
-        read: 'Yes'
-    },
-    {
-        name: 'Think and grow rich',
-        author: 'R C Paul',
-        page: 140,
-        read: 'Yes'
-    }
-];
+var library = [];
 const displayBooks = () => {
     let bookCard = document.getElementById('card');
     bookCard.innerHTML = '';
-    library.forEach(book => {
+    library.forEach((book, index) => {
         const book1 = document.createElement('div');
         book1.style.cssText = 'background-color: #b347b3; border-radius: 5px; padding:2%; margin:2%;  color: #ffffff; font-size:1.5vw;';
         book1.innerHTML = `
@@ -23,30 +10,18 @@ const displayBooks = () => {
          <div>Author - ${book.author}</div>
          <div>Number of pages - ${book.page}</div>
          <div>Read(Yes/No) - ${book.read}</div>
-         <button id="delete" onClick={deleteBook(${book})}>DELETE</button>
+         <button id="delete" onClick={deleteBook(${index})}>DELETE</button>
         `;
         bookCard.appendChild(book1);
         console.log(`${book.name}, ${book.author}, ${book.page}, ${book.read}`);
     });
 };
 
-// const deleteBook = bookName => {
-//     console.log(this);
-//     library.forEach(book => {
-//         if (book === bookName) {
-//             library.remove(book);
-//         }
-//     });
-//     displayBooks();
-// };
-document.querySelector('#delete').addEventListener('click', e => {
-    library.forEach(book => {
-        if (book === e.name) {
-            library.remove(book);
-        }
-    });
+const deleteBook = index => {
+    library.splice(index, 1);
     displayBooks();
-});
+};
+
 const addBookToLibrary = (name, author, page, read) => {
     library.push({ name, author, page, read });
 
