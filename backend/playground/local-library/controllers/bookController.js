c987f1d13de70e4a05fb1e7545411ee570fc3ca6
Nishu Rai <54761473+nishu91020@users.noreply.github.com>
2022-03-05 00:51:36 +0530

    index template not working properly

--- backend/playground/local-library/controllers/bookController.js
+++ backend/playground/local-library/controllers/bookController.js
@@ -1,7 +1,34 @@
 const Book = require('../models/book');
+const Author = require('../models/author');
+const BookInstance = require('../models/bookInstance');
+const Genre = require('../models/genre');
+
+const async = require('async');
 
 exports.index = function (req, res) {
-    res.send('NOT IMPLEMENTED: Site Home Page');
+    async.parallel(
+        {
+            book_count: function (callback) {
+                Book.countDocuments({}, callback);
+            },
+            book_instance_count: function (callback) {
+                BookInstance.countDocuments({}, callback);
+            },
+            book_instance_available: function (callback) {
+                BookInstance.countDocuments({ status: 'Available' }, callback);
+            },
+            author_count: function (callback) {
+                Author.countDocuments({}, callback);
+            },
+            genre_count: function (callback) {
+                Genre.countDocuments({}, callback);
+            }
+        },
+        function (err, results) {
+            console.log(results);
+            res.render('index', { title: 'Local Library Home', error: err, data: results });
+        }
+    );
 };
 exports.book_list = function (req, res) {
     res.send('NOT IMPLEMENTED: book list');
