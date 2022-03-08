61752c65e7953c779c7349acadec4f8bcaacc70a
Nishu Rai <54761473+nishu91020@users.noreply.github.com>
2022-03-09 00:33:21 +0530

    book and book instance create forms

--- backend/playground/local-library/controllers/bookController.js
+++ backend/playground/local-library/controllers/bookController.js
@@ -2,7 +2,7 @@ var Book = require('../models/book');
 var Author = require('../models/author');
 var Genre = require('../models/genre');
 var BookInstance = require('../models/bookinstance');
-
+const { body, validationResult } = require('express-validator');
 var async = require('async');
 
 exports.index = function (req, res) {
@@ -65,11 +65,77 @@ exports.book_detail = function (req, res, next) {
 };
 
 exports.book_create_get = function (req, res) {
-    res.render('book_form', { title: 'Create Book' });
+    async.parallel(
+        {
+            authors: function (cb) {
+                Author.find(cb);
+            },
+            genres: function (cb) {
+                Genre.find(cb);
+            }
+        },
+        function (err, results) {
+            if (err) return next(err);
+            res.render('book_form', { title: 'Create Book', genres: results.genres, authors: results.authors });
+        }
+    );
 };
-exports.book_create_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: book create POST');
-};
+exports.book_create_post = [
+    (req, res, next) => {
+        if (!req.body.genre instanceof Array) {
+            if (typeof req.body.genre === 'undefined') req.body.genre = [];
+            else req.body.genre = new Array(req.body.genre);
+        }
+        next();
+    },
+    body('title', 'title must not be empty').trim().isLength({ min: 1 }).escape(),
+    body('author', 'author must not be empty').trim().isLength({ min: 1 }).escape(),
+    body('summary', 'summary must not be empty').trim().isLength({ min: 1 }).escape(),
+    body('isbn', 'isbn must not be empty').trim().isLength({ min: 1 }).escape(),
+    body('genre.*').escape(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        const book = new Book({
+            title: req.body.title,
+            author: req.body.author,
+            summary: req.body.summary,
+            isbn: req.body.isbn,
+            genre: req.body.genre
+        });
+        if (!errors.isEmpty()) {
+            async.parallel(
+                {
+                    authors: function (cb) {
+                        return Author.find(cb);
+                    },
+                    genres: function (cb) {
+                        return Genre.find(cb);
+                    }
+                },
+                function (err, results) {
+                    if (err) {
+                        return next(err);
+                    }
+                    for (let i = 0; i < results.genres.length; i++) {
+                        if (book.genre.indexOf(results.genres[i]._id) > -1) {
+                            results.genres[i].checked = 'true';
+                        }
+                    }
+                    // console.log(results.authors);
+                    // console.log(results.genres);
+                    res.render('book_form', { title: 'Create Book', authors: results.authors, genres: results.genres, book: book, errors: errors.array() });
+                }
+            );
+            return;
+        }
+        else {
+            book.save(function (err) {
+                if (err) return next(err);
+                res.redirect(book.url);
+            });
+        }
+    }
+];
 exports.book_delete_get = function (req, res) {
     res.send('NOT IMPLEMENTED: book delete GET');
 };
--- backend/playground/local-library/controllers/bookInstanceController.js
+++ backend/playground/local-library/controllers/bookInstanceController.js
@@ -1,4 +1,6 @@
 const BookInstance = require('../models/bookinstance');
+const Book = require('../models/book');
+const { body, validationResult } = require('express-validator');
 
 exports.bookinstance_list = function (req, res, next) {
     BookInstance.find().populate('book').exec(function (err, list_bookinstances) {
@@ -26,11 +28,39 @@ exports.bookinstance_detail = function (req, res, next) {
 };
 
 exports.bookinstance_create_get = function (req, res) {
-    res.render('bookinstance_form', { title: 'Create Book Instance' });
+    Book.find({}, 'title').exec(function (err, books) {
+        if (err) return next(err);
+        res.render('bookinstance_form', { title: 'Create Book Instance', book_list: books });
+    });
 };
-exports.bookinstance_create_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance create POST');
-};
+
+exports.bookinstance_create_post = [
+    body('book').trim().isLength({ min: 1 }).withMessage('Book feild is required').escape(),
+    body('imprint').trim().isLength({ min: 1 }).withMessage('Imprint is required').escape(),
+    body('status').escape(),
+    body('due_back', 'Invalid date').optional({ checkFalsy: true }).isISO8601().toDate(),
+    function (req, res, next) {
+        const errors = validationResult(req);
+        const bookinstance = new BookInstance({
+            book: req.body.book,
+            imprint: req.body.imprint,
+            status: req.body.status,
+            due_back: req.body.due_back
+        });
+        if (!errors.isEmpty()) {
+            Book.find({}, 'title').exec(function (err, books) {
+                if (err) return next(err);
+                res.render('bookinstance_form', { title: 'Create Book Instance', book_list: books, errors: errors.array() });
+            });
+        }
+        else {
+            bookinstance.save(function (err) {
+                if (err) return next(err);
+                res.redirect(bookinstance.url);
+            });
+        }
+    }
+];
 exports.bookinstance_delete_get = function (req, res) {
     res.send('NOT IMPLEMENTED: BookInstance delete GET');
 };
