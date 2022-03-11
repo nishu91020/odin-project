990beedad6bae0f96169f4376d0345ceb15197f4
Nishu Rai <54761473+nishu91020@users.noreply.github.com>
2022-03-11 20:14:44 +0530

    author fully implemented

--- backend/playground/local-library/controllers/authorController.js
+++ backend/playground/local-library/controllers/authorController.js
@@ -117,11 +117,41 @@ exports.author_update_get = function (req, res, next) {
         if (author == null) {
             const err = new Error('author not found');
             err.status = 404;
+            return next(err);
         }
         res.render('author_form', { title: 'Update Author', author: author });
     });
 };
 
-exports.author_update_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: Author update POST');
-};
+exports.author_update_post = [
+    body('first_name')
+        .trim()
+        .isLength({ min: 3 })
+        .withMessage('minimum 3 length first name required')
+        .isAlphanumeric()
+        .withMessage('first name should not contain alphanumeric values!')
+        .escape(),
+    body('family_name').trim().isLength({ min: 1 }).withMessage('last name cannot be empty').isAlphanumeric().withMessage('last name cannot contain alphanumeric!').escape(),
+    body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true }).isISO8601().toDate(),
+    body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true }).isISO8601().toDate(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.render('author_form', { title: 'Create Author', errors: errors.array() });
+        }
+        else {
+            let author = new Author({
+                first_name: req.body.first_name,
+                family_name: req.body.family_name,
+                date_of_birth: req.body.date_of_birth,
+                date_of_death: req.body.date_of_death,
+                _id: req.params.id
+            });
+
+            Author.findByIdAndUpdate(req.params.id, author, {}, function (err, theauthor) {
+                if (err) return next(err);
+                res.redirect(theauthor.url);
+            });
+        }
+    }
+];
